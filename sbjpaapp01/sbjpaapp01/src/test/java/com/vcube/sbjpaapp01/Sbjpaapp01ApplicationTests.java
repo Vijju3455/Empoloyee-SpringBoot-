b5e2c20a454a9d38be7e986e10af73d0e0ad9a81
H Vijaya Lakshmi <151778143+Vijju3455@users.noreply.github.com>
2025-02-13 16:14:17 +0530

    Add files via upload

---
+++ sbjpaapp01/sbjpaapp01/src/test/java/com/vcube/sbjpaapp01/Sbjpaapp01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vcube.sbjpaapp01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sbjpaapp01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
